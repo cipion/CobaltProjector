cfd61754bd7668b75330864fe89b8c732140a865
cipion <thomas.gache@yahoo.fr>
2018-05-31 23:55:19 +0200

    mise en place du service projecteur et de l'ajout d'element

--- www/app-controller/calculs.controller.js
+++ www/app-controller/calculs.controller.js
@@ -5,31 +5,17 @@
         .module('app')
         .controller('CalculsController', CalculsController);
 
-    CalculsController.$inject = ['$location', '$cookies', '$scope', '$uibModal', '$route'];
-    function CalculsController($location, $cookies, $scope, $uibModal, $route) {
+    CalculsController.$inject = ['$location', '$cookies', '$scope', '$uibModal', '$route', 'ProjecteurService'];
+    function CalculsController($location, $cookies, $scope, $uibModal, $route, ProjecteurService) {
          var vm = this;
 		
 		var data = {};
 		vm.ajouterProjecteur = ajouterProjecteur;
 		vm.suprimerProjecteur = suprimerProjecteur;
+		vm.refresh = refresh;
 		
-		var elements = [
-				{
-					"id":"test",
-					"nom":"test"			
-				},
-				{
-					"id":"test1",
-					"nom":"test1"			
-				},
-				{
-					"id":"test2",
-					"nom":"test2"			
-				}
-			
-			]
 		
-		$scope.elements = angular.copy(elements);
+		$scope.elements = angular.copy(ProjecteurService.elements);
 		
 			
 			
@@ -44,13 +30,15 @@
 			backdrop: true,
 			keyboard: true,
 			animation: true,			
-			controller: ['$scope', '$uibModalInstance', 
-				function($scope, $uibModalInstance, elements) { //Controller de la fenêtre. Il doit prend en paramètre tous les élèments du "resolve".
+			controller: ['$scope', '$uibModalInstance', 'ProjecteurService',
+				function($scope, $uibModalInstance, ProjecteurService) { //Controller de la fenêtre. Il doit prend en paramètre tous les élèments du "resolve".
 					$scope.ajouter = function() {
 						//On fait appel à la fonction du scope parent qui permet de supprimer l'élément.
 						//On peut également faire appel à un service de notre application.
-						elements={"id":"test3", "nom":"test3"}
+						ProjecteurService.addElement({"id":$scope.nomProjecteur, "nom":$scope.nomProjecteur});
 						//Fermeture de la fenêtre modale
+						
+						refresh();
 						$uibModalInstance.close();
 					};
 					$scope.cancel = function() {
@@ -89,10 +77,15 @@
 			
 			// delete elements[objectDelete];
 			
-			elements.splice(objectDelete, 1);
-			$scope.elements = angular.copy(elements);
+			ProjecteurService.suppElement(objectDelete);
+			refresh();
 		};
 		
+		
+		function refresh ()
+		{
+			$scope.elements = angular.copy(ProjecteurService.elements);
+		}
 		
 		
 		
--- www/app-services/projecteur.service.js
+++ www/app-services/projecteur.service.js
@@ -8,14 +8,40 @@
     ProjecteurService.$inject = [ '$http', '$cookies', '$rootScope', '$timeout'];
     function ProjecteurService($http, $cookies, $rootScope, $timeout) {
         var service = {};
-
+		
+		
+		service.elements = [
+				{
+					"id":"test",
+					"nom":"test"			
+				},
+				{
+					"id":"test1",
+					"nom":"test1"			
+				},
+				{
+					"id":"test2",
+					"nom":"test2"			
+				}
+			
+			]
         
-        
-
+        service.addElement = addElement;
+		service.suppElement = suppElement;
+		
         return service;
 
         
-
+		function addElement(element)
+		{
+			service.elements.push(element);
+		
+		}
+		
+		function suppElement(indexElement)
+		{
+			service.elements.splice(indexElement, 1);
+		}
         
 
        
