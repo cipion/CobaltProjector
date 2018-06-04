bf3ab999bad8dad4b7b7011189c671fbcbbdaef0
cipion <thomas.gache@yahoo.fr>
2018-06-04 23:49:16 +0200

    commit test BDD 2

--- www/app-services/projecteur.service.js
+++ www/app-services/projecteur.service.js
@@ -10,28 +10,16 @@
         var service = {};
 		
 		
-		service.elements = [
-				{
-					"id":"test",
-					"nom":"test"			
-				},
-				{
-					"id":"test1",
-					"nom":"test1"			
-				},
-				{
-					"id":"test2",
-					"nom":"test2"			
-				}
-			
-			]
-		
+				
 		var db = window.openDatabase("Database", "1.0", "Cordova Demo", 200000);
 		db.transaction(populateDB, errorCB, successCB);
-        
+        service.elements = angular.copy(showDocCount(db));
+		
+		
         service.addElement = addElement;
 		service.suppElement = suppElement;
 		service.populateDB = populateDB;
+		service.showDocCount = showDocCount;
 		
         return service;
 
@@ -50,10 +38,12 @@
 		
 		
 		function populateDB(tx) {
-			tx.executeSql('DROP TABLE IF EXISTS DEMO');
-			tx.executeSql('CREATE TABLE IF NOT EXISTS DEMO (id unique, data)');
-			tx.executeSql('INSERT INTO DEMO (id, data) VALUES (1, "First row")');
-			tx.executeSql('INSERT INTO DEMO (id, data) VALUES (2, "Second row")');
+			//tx.executeSql('DROP TABLE IF EXISTS DEMO');
+			tx.executeSql('CREATE TABLE IF NOT EXISTS DEMO (id unique, nom)');
+			tx.executeSql('INSERT INTO DEMO (id, nom) VALUES (1, "test1")');
+			tx.executeSql('INSERT INTO DEMO (id, nom) VALUES (2, "test2")');
+			tx.executeSql('INSERT INTO DEMO (id, nom) VALUES (3, "test3")');
+			tx.executeSql('INSERT INTO DEMO (id, nom) VALUES (4, "test4")');
 		}
 		
 		function errorCB(err) {
@@ -64,7 +54,16 @@
 			alert("success!");
 		}
 		
-		
+		function showDocCount(db) {
+		db.readTransaction(function (t) {
+			t.executeSql('SELECT * FROM DEMO', [], function (t, r) {
+			return r.rows;
+			}, function (t, e) {
+			// couldn't read database
+			console.log('erreur ' + e.message );
+			});
+		});
+}
         
 
        
